9da33d2495c8d171d64694cc2039e62dd5f52dd7
mmraihan <mm_raihan@live.com>
2021-10-21 00:56:31 +0600

    Type Basic

--- output/script.js
+++ output/script.js
@@ -3,3 +3,15 @@
 /* let palyerName="Sakib AL Hasan";
 let palyerName=1;
 console.log(palyerName); */
+// Remcommend define Tyepe
+function name2(a, b) {
+    return a + b;
+}
+console.log(name2(2, 2));
+//Object//
+var person = {
+    name: "Sakib  hasan",
+    age: 35,
+    isCaptain: true,
+};
+person.name = "Sakib AL Hasan"; // in object you can't assign  number type. ALready set a structure when you declared
--- src/script.ts
+++ src/script.ts
@@ -4,3 +4,17 @@
 let palyerName=1;
 console.log(palyerName); */
 
+// Remcommend define Tyepe
+function name2(a: number, b: number) {
+    return a+b;
+}
+console.log(name2(2,2));
+
+//Object//
+let person={
+    name : "Sakib  hasan",
+    age:35,
+    isCaptain: true,
+
+}
+person.name="Sakib AL Hasan"; // in object you can't assign  number type. ALready set a structure when you declared
