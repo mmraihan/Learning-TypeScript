55b9a15defc5ad207ad4901798a7698ab29518d3
mmraihan <mm_raihan@live.com>
2021-10-21 02:25:53 +0600

    Dynamic or Any Type

---
+++ output/DynamicOrAnyType.js
@@ -0,0 +1,15 @@
+"use strict";
+// Use wisely "any", not recommended
+var anyA;
+anyA = 5;
+anyA = 'Raihan';
+//Array
+var dynamicArray = [];
+dynamicArray.push(12);
+dynamicArray.push("raihan");
+//Object
+var dynamicObj;
+dynamicObj = {
+    name: "Mr Alam",
+    age: 22
+};
---
+++ src/DynamicOrAnyType.ts
@@ -0,0 +1,29 @@
+
+// Use wisely "any", not recommended
+let anyA : any;
+anyA=5;
+anyA='Raihan';
+
+ //Array
+let dynamicArray : any []=[];
+
+dynamicArray.push(12);
+dynamicArray.push("raihan");
+
+
+//Object
+
+let dynamicObj : {
+    name: any,
+    age : any,
+};
+
+
+dynamicObj={
+    name: "Mr Alam",
+    age: 22
+};
+
+
+
+
