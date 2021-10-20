a8d5209d26e9c53ba83346f8cdbdb809dab1a7d9
mmraihan <mm_raihan@live.com>
2021-10-21 00:31:42 +0600

    Question-1

--- src/script.ts
+++ src/script.ts
@@ -1,37 +1,6 @@
-/* let a: string;
-let b:number;
 
-a= "Raihan"
-b=3;
+//---------------Question -1 in Output--------------
+/* let palyerName="Sakib AL Hasan";
+let palyerName=1;
+console.log(palyerName); */
 
- */
-//Array
-
-/* let a:string[]=[];
-a.push('Raihan');
-
-
-let b: (string | number)[]=[];
-b.push("oooo",4) */
-
-//Object
-/* 
-let c:{
-    name: string,
-    age: number,
-    adult: boolean
-};
-c={
-    name: "Raihan",
-    age:24,
-    adult:false
-}
-console.log(c);
- */
-
-let myFunc: Function;
-myFunc=( a: string, b:string, c?:string)=>{
-    console.log(`Hello ${a}, $(b)`);
-}
-
-myFunc('An', 'bb','c');
