cc1f4c7b8ee712b31956eb367210c3593a8ef695
mmraihan <mm_raihan@live.com>
2021-10-21 01:18:35 +0600

    String Interpolation and Explicit Type

---
+++ output/explicitAnsUnion.js
@@ -0,0 +1,7 @@
+"use strict";
+// ----------------------- Explicit & Union Types-------------------
+var a;
+var b;
+a = "Mubasshir";
+b = 24;
+console.log("Hello " + a + " ! I am " + b + " years old"); // String Interpolation
---
+++ src/explicitAnsUnion.ts
@@ -0,0 +1,9 @@
+// ----------------------- Explicit & Union Types-------------------
+
+let a:string;
+let b: number;
+
+a="Mubasshir"
+b=24;
+console.log(`Hello ${a} ! I am ${b} years old`); // String Interpolation
+
