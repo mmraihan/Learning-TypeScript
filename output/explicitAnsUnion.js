a152b9f658d9678130f2404fad076bd9b56993ec
mmraihan <mm_raihan@live.com>
2021-10-21 01:58:05 +0600

    Explicit & Union Types

--- output/explicitAnsUnion.js
+++ output/explicitAnsUnion.js
@@ -1,7 +1,20 @@
 "use strict";
-// ----------------------- Explicit & Union Types-------------------
+// ----------------------- Explicit-------------------
 var a;
 var b;
 a = "Mubasshir";
 b = 24;
 console.log("Hello " + a + " ! I am " + b + " years old"); // String Interpolation
+//-------------Union in Array-------------
+var c = [];
+c.push('Raihan');
+c.push(3);
+c.forEach(function (e) {
+    console.log(e);
+});
+//-------------Union in Object-------------
+var anObject;
+anObject = {
+    name: "Raihan",
+    cgpa: 3.01,
+};
--- src/explicitAnsUnion.ts
+++ src/explicitAnsUnion.ts
@@ -1,4 +1,4 @@
-// ----------------------- Explicit & Union Types-------------------
+// ----------------------- Explicit-------------------
 
 let a:string;
 let b: number;
@@ -6,4 +6,34 @@ let b: number;
 a="Mubasshir"
 b=24;
 console.log(`Hello ${a} ! I am ${b} years old`); // String Interpolation
+
+
+//-------------Union in Array-------------
+
+let c: (string | number) []=[]
+c.push('Raihan');
+c.push(3);
+
+
+c.forEach(e => {
+    console.log(e)
+    
+});
+//-------------Union in Object-------------
+
+
+let anObject : {
+    name: string,
+    cgpa : number,
+}
+
+anObject={
+    name:"Raihan",
+    cgpa:3.01,
+}
+
+
+
+
+
 
